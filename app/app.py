d09fb4fb7dc57cab6b5dad303881dd6441a39dc1
hehe012 <zhanghy523@mail2.sysu.edu.cn>
2025-06-06 21:00:37 +0800

    0606

--- app/app.py
+++ app/app.py
@@ -3,9 +3,11 @@ from utils.generate_recipe import generate_recipe
 from utils.generate_image_description import generate_image_description
 from utils.generate_dish_image import generate_dish_image
 from utils.generate_steps_image import generate_steps_image
+from utils.generate_nutritional_analysis import generate_nutrition_analysis
 from PIL import Image
 import requests
 import os
+
 import base64
 from io import BytesIO
 
@@ -22,12 +24,12 @@ def generate():
     cuisine_type = request.form.get('cuisine_type')
     
     try:
-        # 生成菜谱
         recipe = generate_recipe(
             ingredients=ingredients,
             cuisine_type=cuisine_type
         )
         
+
         # 生成菜品外貌描述
         appearance_desc = generate_image_description(recipe)
         img = generate_dish_image(appearance_desc)
@@ -36,8 +38,10 @@ def generate():
         img.save(buffered, format="PNG")
         img_base64 = base64.b64encode(buffered.getvalue()).decode('utf-8')
         
+        dish_nutrition=generate_nutrition_analysis(recipe)
         # 生成每个步骤的图片描述
         step_imgs = generate_steps_image(recipe)
+        
         step_base64 = []
         for image in step_imgs:
             buffered = BytesIO()
@@ -49,7 +53,8 @@ def generate():
         return jsonify({
             "recipe": recipe,
             "steps_images": step_base64,
-            "dish_image": img_base64
+            "dish_image": img_base64, 
+            "nutrition": dish_nutrition 
         })
     except Exception as e:
         return jsonify({"error": str(e)}), 500
