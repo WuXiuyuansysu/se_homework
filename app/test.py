b142636e9946a966cbd00f140de40b1f714b5391
wxy <wuxy357@mail2.sysu.edu.cn>
2025-06-19 20:50:30 +0800

    update generate_steps_image

--- app/test.py
+++ app/test.py
@@ -1,7 +1,7 @@
 
 from classes.RecipeGenerationPipeline import RecipeGenerationPipeline
 if __name__ == "__main__":
-    ingredients = "面条，大便，牛肉，猪肉，木耳"
+    ingredients = "辣椒、猪肉、白菜、虾仁、鸡蛋、玉米粒"
     cuisine_type = "中式粤菜"
 
     pipline = RecipeGenerationPipeline(ingredients, cuisine_type)
